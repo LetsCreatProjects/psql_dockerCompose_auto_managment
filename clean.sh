9861c0f52139f3575444cfdb0117fa7aff335fc6
Elina Karshigeiv <elina2412@yahoo.com>
2022-07-18 00:34:53 -0400

    clean file edition

--- clean.sh
+++ clean.sh
@@ -3,4 +3,7 @@
 docker rm -f $(docker ps -aq) && docker system prune -y
 sudo docker rmi $(sudo docker images -aq) --force 
 
+docker network prune
+
 sudo docker ps && sudo docker images && sudo docker volume
+
